76bb5cdda2cf776a14e3a9c9d7e6969427f0ae97
muhammadanaswayanad <muhammadanaswayanad@gmail.com>
2025-10-26 18:05:23 +0530

    smarthive_client: enforce SmartHive block on authenticate and exempt client admins in access checks

--- models/res_users.py
+++ models/res_users.py
@@ -10,17 +10,43 @@ CLIENT_CONFIG_MODEL = 'smarthive.client.config'
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
+    @classmethod
+    def authenticate(cls, db, login, password, user_agent_env):
+        """Override authenticate to check SmartHive access restrictions"""
+        # First perform normal authentication
+        uid = super(ResUsers, cls).authenticate(db, login, password, user_agent_env)
+        
+        if uid:
+            # Check SmartHive restrictions after successful authentication
+            with cls.pool.cursor() as cr:
+                env = api.Environment(cr, uid, {})
+                config = env[CLIENT_CONFIG_MODEL].get_active_config()
+                
+                if config and config.is_blocked:
+                    user = env['res.users'].browse(uid)
+                    
+                    # Allow access for superuser, system admins, and SmartHive Client Admins
+                    if not (uid == 1 or 
+                           user.has_group('base.group_system') or 
+                           user.has_group('smarthive_client.group_smarthive_client_admin')):
+                        block_reason = config.block_reason or "System access is currently restricted. Contact your administrator."
+                        raise AccessDenied(_("Access Denied: %s") % block_reason)
+        
+        return uid
+
     @api.model
     def check_access_rights(self, operation, raise_exception=True):
         """Override to enforce SmartHive access restrictions"""
         # First check normal access rights
         result = super(ResUsers, self).check_access_rights(operation, raise_exception=raise_exception)
         
-        # Skip check for superuser and system operations
-        if self.env.uid == 1 or self.env.user.has_group('base.group_system'):
+        # Skip check for superuser, system admins, and SmartHive Client Admins
+        if (self.env.uid == 1 or 
+            self.env.user.has_group('base.group_system') or 
+            self.env.user.has_group('smarthive_client.group_smarthive_client_admin')):
             return result
             
-        # Check SmartHive restrictions for non-admin users
+        # Check SmartHive restrictions for other users
         config = self.env[CLIENT_CONFIG_MODEL].get_active_config()
         if config and config.is_blocked:
             if raise_exception:
